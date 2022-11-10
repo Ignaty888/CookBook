cd0be3ddf18830b9d5464c24a75c8444d27c3e6b
Ignaty_len <ignaty888@mail.ru>
2022-11-10 13:08:17 +0300

    addDishCard

--- view/MainPage.jsx
+++ view/MainPage.jsx
@@ -1,11 +1,18 @@
 const React = require('react');
 const Layout = require('./Layout');
 const Navbar = require('./Navbar');
+const DishCard = require('./DishCard')
 
-module.exports = function MainPage({ user, title }) {
+module.exports = function MainPage({ user, title, dishes }) {
   return (
     <Layout title={title}>
       <Navbar user={user} />
+      <div className='container d-flex row p-2 justify-content-center alogn-items-center m-3 ' >
+        {dishes.map((dish) => {
+          return <DishCard key={dish.id} dish={dish} />
+        })}
+      </div>
+
     </Layout>
   );
 };
