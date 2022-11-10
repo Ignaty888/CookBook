2351cb9c5aade9654a4c0432bbf8429525f2e952
Ignaty_len <ignaty888@mail.ru>
2022-11-10 20:18:56 +0300

    frontFix

--- view/MainPage.jsx
+++ view/MainPage.jsx
@@ -1,14 +1,14 @@
 const React = require('react');
 const Layout = require('./Layout');
 const Navbar = require('./Navbar');
-const DishCard = require('./DishCard')
+const DishCard = require('./DishCard');
 
 module.exports = function MainPage({ user, title, dishes }) {
   return (
     <Layout title={title}>
       <Navbar user={user} />
-      
-      <div className='container d-flex row p-2 justify-content-center alogn-items-center mt-3 ' >
+
+      <div className="container d-flex row p-2 justify-content-center alogn-items-center mt-3 ">
         <select className="form-select" aria-label="Default select example">
           <option selected>Sort by</option>
           <option value="1">Time desc ⬆</option>
@@ -16,9 +16,7 @@ module.exports = function MainPage({ user, title, dishes }) {
           <option value="3">Ingredient desc ⬆</option>
           <option value="4">Ingredient asc ⬇</option>
         </select>
-        {dishes.map((dish) => {
-          return <DishCard key={dish.id} dish={dish} user = {user}/>
-        })}
+        {dishes.map((dish) => <DishCard key={dish.id} dish={dish} user={user} />)}
       </div>
 
     </Layout>
